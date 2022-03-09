e906086948026237bcd7b5b1954499275b2e16bb
root <root@ip-172-31-19-150.ec2.internal>
2022-03-09 10:38:29 +0000

    this is sceond commit

--- subhashini.tf
+++ subhashini.tf
@@ -3,6 +3,13 @@ resion = "us-east-1"
 access_key = ""
 secret_key = ""
 }
+resource "aws_install" "main" {
+instance_type = ""
+ami = ""
+availability_zone = "us-east-1"
+key_name = "suu"
+}
+
 resource "aws_s3_bucket" "abc" {
 bucker = "lakshmi456lakshmi"
 acl =" private"
