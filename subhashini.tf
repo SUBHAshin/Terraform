562bf8bbda9f00bbe18088d5d42d64ed8be29d1c
root <root@ip-172-31-19-150.ec2.internal>
2022-03-09 10:49:24 +0000

    tins is six commit

--- subhashini.tf
+++ subhashini.tf
@@ -3,14 +3,7 @@ resion = "us-east-1"
 access_key = ""
 secret_key = ""
 }
-resource "aws_install" "main" {
-instance_type = ""
-ami = ""
-availability_zone = "us-east-1"
-key_name = "suu"
-}
-
-resource "aws_s3_bucket" "abc" {
+resource "aws_s3_bucket" "def" {
 bucker = "lakshmi456lakshmi"
 acl =" private"
 }
