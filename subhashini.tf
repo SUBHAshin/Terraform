e444d9a237374febc0bbc523a9baceba987df42a
SUBHAshin <98635577+SUBHAshin@users.noreply.github.com>
2022-03-14 12:14:07 +0530

    Update subhashini.tf

--- subhashini.tf
+++ subhashini.tf
@@ -4,6 +4,6 @@ access_key = "AKIAVMFWVN35HVGCKG3C"
 secret_key = "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"
 }
 resource "aws_s3_bucket" "def" {
-bucker = "lakshmi456lakshmi"
+bucker = "lakhami456lakshmi"
 acl =" private"
 }
